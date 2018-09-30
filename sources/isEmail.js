8d1f4c169d49049a331a8d6f4bffa79346a607d0
Sylvain LONGEPEE <slongepee@gmail.com>
2018-09-30 10:42:35 +0200

    add examples

--- sources/isEmail.js
+++ sources/isEmail.js
@@ -1,5 +1,6 @@
 function isEmail(input) {
-  return !/\s/.test(input) &&
+  return !/@.*@.*/.test(input) &&
+    !/\s/.test(input) &&
     !/\.@/.test(input) &&
     !/\.\./.test(input) &&
     /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/
